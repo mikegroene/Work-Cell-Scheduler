e7dacff50e6e06bd00a3515771c2f818ecb9be90
mg2m5 <mg2m5@mail.missouri.edu>
2014-03-04 13:18:26 -0600

    Copied assignment from official

---
+++ Web/assignment.php
@@ -0,0 +1,23 @@
+<?php
+namespace WCS;
+require_once 'Work-Cell-Scheduler/Config/global.php';
+
+class TrainingMatrix{
+
+	private $people = array('Ann','Bob','Clide');
+	private $line = array(1010,1020,1030,1040);
+
+	public function getPeople() {
+		return $this->people;
+	}
+
+	public function getWorkstations() {
+		return $this->line;
+	}
+
+	public function getTraining($person,$workstation){
+		return 0.10;
+	}
+
+}
+?>
